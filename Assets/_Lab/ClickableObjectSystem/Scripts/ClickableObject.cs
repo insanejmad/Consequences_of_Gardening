3084b257be3efbe30d0362099a9eccbf814205c3
Kévin Reilhac <kreilhac@xeno-bits.com>
2020-04-18 16:33:35 +0200

    Clickable Object start

---
+++ Assets/_Lab/ClickableObjectSystem/Scripts/ClickableObject.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ClickableObject : MonoBehaviour
+{
+    [SerializeField] private bool isTakeble = true;
+    [SerializeField] private GameObject highlightObject = null;
+
+    private bool _isHovered = false;
+
+    private const float _takeAnimationTime = 0.2f;
+
+    private void Awake()
+    {
+        if (!GetComponent<Collider2D>()) {
+            Debug.LogWarning(name + " need a 2D collider");
+            gameObject.AddComponent<Collider2D>();
+        }
+	HoveredOff();
+    }
+
+    private void Update()
+    {
+	if (_isHovered && isTakeble && Input.GetMouseButtonDown(0))
+	{
+	    ClickableObjectManager.instance.OpenPanel(this);
+	}
+    }
+
+    public void Take()
+    {
+	isTakeble = false;
+	StartCoroutine(TakeAnimation());
+    }
+
+    public IEnumerator TakeAnimation()
+    {
+	Vector2 startPosition = transform.position;
+	Vector2 endPosition = ClickableObjectManager.instance.GetItemTargetPos;
+
+	for (float t = 0; t < _takeAnimationTime; t += Time.deltaTime)
+	{
+	    transform.position = Vector2.Lerp(startPosition, endPosition, t / _takeAnimationTime);
+	    yield return null;
+	}
+	gameObject.SetActive(false);
+    }
+
+    public void Inspect()
+    {
+	Debug.Log(name + " Inspected");
+    }
+
+    public bool IsHovered
+    {
+        get => _isHovered;
+    }
+
+    private void OnMouseOver()
+    {
+	if (!isTakeble)
+	    return;
+	_isHovered = true;
+	HoveredOn();
+    }
+
+    private void OnMouseExit()
+    {
+	if (!isTakeble)
+	    return;
+	_isHovered = false;
+	HoveredOff();
+    }
+
+    private void HoveredOff()
+    {
+	if (highlightObject)
+	    highlightObject.SetActive(false);
+    }
+
+    private void HoveredOn()
+    {
+	if (highlightObject)
+	    highlightObject.SetActive(true);
+    }
+}
---
+++ Assets/_Lab/ClickableObjectSystem/Scripts/ClickableObjectManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickableObjectManager : MonoBehaviour
+{
+    public static ClickableObjectManager instance = null;
+    [SerializeField] RectTransform ChoicePanel = null;
+
+    [Tooltip("End point for take animation")] [SerializeField]
+    private Transform itemTarget = null;
+
+    private bool _isTargetOnUI = false;
+    private ClickableObject _target;
+
+    private void Awake()
+    {
+	instance = this;
+	if (itemTarget == null)
+	    Debug.LogWarning(name + " need an itemTarget");
+	else
+	    _isTargetOnUI = itemTarget.GetComponent<RectTransform>() != null;
+    }
+
+    private void Start()
+    {
+	ChoicePanel.gameObject.SetActive(false);
+    }
+
+
+    public Vector2 GetItemTargetPos
+    {
+	get
+	{
+	    if (itemTarget == null)
+		return Vector2.zero;
+	    if (_isTargetOnUI)
+		return Camera.main.ScreenToWorldPoint(itemTarget.position);
+	    return itemTarget.position;
+	}
+    }
+
+    public void OpenPanel(ClickableObject target)
+    {
+	ChoicePanel.position = Input.mousePosition;
+	ChoicePanel.gameObject.SetActive(true);
+	_target = target;
+    }
+
+    public void Take()
+    {
+	ChoicePanel.gameObject.SetActive(false);
+	_target.Take();
+	_target = null;
+    }
+
+    public void Inspect()
+    {
+	ChoicePanel.gameObject.SetActive(false);
+	_target.Inspect();
+	_target = null;
+    }
+}
