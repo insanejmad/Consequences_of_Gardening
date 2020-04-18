54fd9cf045d6ee259122a6a08028f5ea424e7f36
Kevin <unfricht.kevin@hotmail.fr>
2020-04-18 17:49:05 +0200

    NEW (dialog system) : Base UI and simple reader

---
+++ Assets/_Lab/dialog_system/Scripts/TMP_Animated/Runtime/TMP_Animated.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TMPro {
+    public class TMP_Animated : TextMeshProUGUI
+    {
+        [SerializeField] public float speed = 10;
+
+        public void Clear() {
+            text = string.Empty;
+            maxVisibleCharacters = 0;
+        }
+
+        public void Read(string textToRead) {
+            Clear();
+            text = textToRead;
+
+            StartCoroutine(DisplayText());
+
+            IEnumerator DisplayText() {
+                int counter = 0;
+                while (counter < text.Length) {
+                    counter++;
+                    maxVisibleCharacters++;
+                    yield return new WaitForSeconds(1f / speed);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/_Lab/dialog_system/Scripts/UIDialogManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+using TMPro;
+
+namespace DialogSystem
+{
+    [RequireComponent(typeof(CanvasGroup))]
+    public class UIDialogManager : MonoBehaviour
+    {
+        public static UIDialogManager instance;
+
+        public TMP_Animated text;
+
+        CanvasGroup canvasGroup;
+
+        void Awake() {
+            canvasGroup = GetComponent<CanvasGroup>();
+            FadeUI(false, 0, 0);
+        }
+
+        void Update() {
+            if (Input.GetMouseButtonDown(0)) {
+                FadeUI(true, .2f, .65f);
+            }
+        }
+
+        public void FadeUI(bool show, float time, float delay)
+        {
+            Sequence s = DOTween.Sequence();
+            s.AppendInterval(delay);
+            s.Append(canvasGroup.DOFade(show ? 1 : 0, time));
+            if (show)
+            {
+                text.Clear();
+                s.Join(canvasGroup.transform.DOScale(0, time * 2).From().SetEase(Ease.OutBack));
+                s.AppendCallback(() => text.Read("Ceci est un text à lire, avec de la ponctuation. C'est un peu compliqué..."));
+            }
+        }
+
+    }
+}
