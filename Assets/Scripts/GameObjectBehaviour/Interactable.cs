dec38c24835c62af3c5305495347c844529cb36f
Kevin <unfricht.kevin@hotmail.fr>
2020-04-19 19:34:27 +0200

    FEAT (room) : outline selectable + interactable launch simple dialog

--- Assets/Scripts/GameObjectBehaviour/Interactable.cs
+++ Assets/Scripts/GameObjectBehaviour/Interactable.cs
@@ -7,17 +7,38 @@ namespace GameObjectBehavior
 {
     [System.Serializable] public class InteractableEvent : UnityEvent {}
 
+    [RequireComponent(typeof(SpriteRenderer))]
     [RequireComponent(typeof(Rigidbody2D))]
     [RequireComponent(typeof(BoxCollider2D))]
     public class Interactable : MonoBehaviour
     {
         public InteractableEvent OnInteracted;
         public bool IsHovered { get; private set; } = false;
+        public Material HoverMaterial;
+
+        Material OriginalMaterial;
+        SpriteRenderer Sprite;
+
+        void Awake()
+        {
+            Sprite = GetComponent<SpriteRenderer>();
+        }
 
         void Update()
         {
             if (Input.GetMouseButtonDown(0))
                 OnInteracted.Invoke();
+
+            if (IsHovered && null == OriginalMaterial)
+            {
+                OriginalMaterial = Sprite.material;
+                Sprite.material = HoverMaterial;
+            }
+            else if (!IsHovered && null != OriginalMaterial)
+            {
+                Sprite.material = OriginalMaterial;
+                OriginalMaterial = null;
+            }
         }
 
         void OnMouseEnter()
