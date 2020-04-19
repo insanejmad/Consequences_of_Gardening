2089628d113203d6c129c4306f08e9e442ee6561
Kevin <unfricht.kevin@hotmail.fr>
2020-04-19 18:59:54 +0200

    NEW (room scene) : base room with interactable cat

---
+++ Assets/Scripts/DialogSystem/TriggerSimpleDialog.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameObjectBehavior;
+
+namespace DialogSystem
+{
+    [RequireComponent(typeof(Interactable))]
+    public class TriggerSimpleDialog : MonoBehaviour
+    {
+        public Dialog Dialog;
+
+        Interactable Interactable;
+
+        void Awake()
+        {
+            Interactable = GetComponent<Interactable>();
+        }
+
+        void OnEnable()
+        {
+            Interactable.OnInteracted.AddListener(TriggerDialog);
+        }
+
+        void OnDisable()
+        {
+            Interactable.OnInteracted.RemoveListener(TriggerDialog);
+        }
+
+        void TriggerDialog()
+        {
+            if (UIDialogManager.Instance.InDialog)
+                return;
+
+            UIDialogManager.Instance.Dialog = Dialog;
+        }
+    }
+}
---
+++ Assets/Scripts/GameObjectBehaviour/Interactable.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace GameObjectBehavior
+{
+    [System.Serializable] public class InteractableEvent : UnityEvent {}
+
+    [RequireComponent(typeof(Rigidbody2D))]
+    [RequireComponent(typeof(BoxCollider2D))]
+    public class Interactable : MonoBehaviour
+    {
+        public InteractableEvent OnInteracted;
+        public bool IsHovered { get; private set; } = false;
+
+        void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+                OnInteracted.Invoke();
+        }
+
+        void OnMouseEnter()
+        {
+            IsHovered = true;
+        }
+
+        void OnMouseExit()
+        {
+            IsHovered = false;
+        }
+    }   
+}
