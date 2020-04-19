453961139bdaf77ff107efb4e33de6802bdcac10
Kévin Reilhac <kreilhac@xeno-bits.com>
2020-04-19 15:43:51 +0200

    Add ClickableObjectManager & ChoicePanel prefabs

---
+++ Assets/_Lab/ClickableObjectSystem/Scripts/ChoicePanel.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChoicePanel : MonoBehaviour
+{
+    [SerializeField] private Button takeButton = null;
+    [SerializeField] private Button inspectButton = null;
+    [SerializeField] private Text itemNameLabel = null;
+
+    private ClickableObject _target = null;
+    private RectTransform _rectTransform = null;
+    private string _startTakeButtonString = null;
+    private Text _takeButtontextLabel = null;
+    private Canvas _canvas = null;
+    private RectTransform _canvasRectTransform = null;
+
+    private const string _inventoryFullString = "Votre inventaire est plein.";
+
+    public bool HaveCursorOn
+    {
+        get
+        {
+            if (!gameObject.activeSelf)
+                return (false);
+            Camera camera = _canvas.renderMode == RenderMode.ScreenSpaceOverlay ? null : Camera.main;
+
+            return (RectTransformUtility.RectangleContainsScreenPoint(_rectTransform, Input.mousePosition, camera));
+        }
+    }
+
+    public void Setup(ClickableObject target)
+    {
+        if (target == null || !target.IsInteractable)
+            return;
+        _target = target;
+        PlacePanelOnScreen();
+        takeButton.gameObject.SetActive(target.isTakeble);
+        UpdateTakeButton();
+        inspectButton.gameObject.SetActive(target.isInspectable);
+        itemNameLabel.text = target.Item.ItemName;
+        gameObject.SetActive(true);
+    }
+
+
+    private void Awake()
+    {
+        _rectTransform = GetComponent<RectTransform>();
+        _canvas = gameObject.GetComponentInParent<Canvas>();
+        _canvasRectTransform = _canvas.GetComponent<RectTransform>();
+        if (takeButton)
+            _takeButtontextLabel = takeButton.GetComponentInChildren<Text>();
+        if (_takeButtontextLabel)
+            _startTakeButtonString = _takeButtontextLabel.text;
+    }
+
+    private void Start()
+    {
+        takeButton.onClick.AddListener(Take);
+        inspectButton.onClick.AddListener(Inspect);
+        gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (!HaveCursorOn && Input.GetMouseButtonDown(0))
+            ResetPanel();
+    }
+
+    private void Take()
+    {
+        _target.Take();
+        ResetPanel();
+    }
+
+    private void Inspect()
+    {
+        _target.Inspect();
+        ResetPanel();
+    }
+
+    private void ResetPanel()
+    {
+        gameObject.SetActive(false);
+        itemNameLabel.text = "";
+        _target = null;
+    }
+
+    private void UpdateTakeButton()
+    {
+        takeButton.interactable = CanAddItemInInventory;
+        _takeButtontextLabel.text = CanAddItemInInventory ? _startTakeButtonString : _inventoryFullString;
+    }
+
+    private bool CanAddItemInInventory
+    {
+        get => (PlayerInventory.instance.ItemList.Count < PlayerInventory.instance.maxItems);
+    }
+
+    private void PlacePanelOnScreen()
+    {
+        float xOverLap = 0f;
+        float yOverLap = 0f;
+
+        transform.position = Input.mousePosition;
+        if (_rectTransform.anchoredPosition.x + _rectTransform.sizeDelta.x > _canvasRectTransform.sizeDelta.x / 2)
+            xOverLap = _rectTransform.anchoredPosition.x + _rectTransform.sizeDelta.x - _canvasRectTransform.sizeDelta.x / 2;
+        if (_rectTransform.anchoredPosition.y > _canvasRectTransform.sizeDelta.y / 2)
+            yOverLap = _rectTransform.anchoredPosition.y - _canvasRectTransform.sizeDelta.y / 2;
+        if (_rectTransform.anchoredPosition.y - _rectTransform.sizeDelta.y < -_canvasRectTransform.sizeDelta.y / 2)
+            yOverLap = _rectTransform.anchoredPosition.y - _rectTransform.sizeDelta.y + _canvasRectTransform.sizeDelta.y / 2;
+        _rectTransform.anchoredPosition -= new Vector2(xOverLap, yOverLap);
+    }
+}
---
+++ Assets/_Lab/ClickableObjectSystem/Scripts/ClickableObject.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ClickableObject : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer spriteRenderer = null;
+    public bool isTakeble = true;
+    public bool isInspectable = true;
+    [SerializeField] private GameObject highlightObject = null;
+    [SerializeField] private Item item = null;
+
+    private bool _isHovered = false;
+
+    private const float _takeAnimationTime = 0.2f;
+
+    public bool IsInteractable
+    {
+        get => (isInspectable || isTakeble);
+    }
+
+    public Item Item
+    {
+        get => item;
+    }
+
+    private void Awake()
+    {
+        if (!GetComponent<Collider2D>()) {
+            Debug.LogWarning(name + " need a 2D collider :c", this);
+            gameObject.AddComponent<Collider2D>();
+        }
+        if (!spriteRenderer)
+            spriteRenderer = GetComponent<SpriteRenderer>();
+        if (item == null) {
+            Debug.LogError(name + " need an item \\o/");
+            return;
+        }
+        HoverOff();
+    }
+
+    private void Start()
+    {
+        SetupItem();
+    }
+
+    private void SetupItem()
+    {
+        if (item == null)
+            return;
+        if (item.Sprite != null)
+            spriteRenderer.sprite = item.Sprite;
+        //Wait PlayerInventoryInstantiationInAwaik
+        //if (PlayerInventory.instance.ItemList.Contains(Item))
+        //    gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        if (_isHovered && ClickableObjectManager.HaveInstance) {
+            if (ClickableObjectManager.instance.CursorOnChoicePanel)
+                HoverOff();
+            if (Input.GetMouseButtonDown(0))
+                ClickableObjectManager.instance.OpenChoicePanel(this);
+        }
+    }
+
+    #region INTERACTIONS
+    public void Take()
+    {
+        isTakeble = false;
+        StartCoroutine(TakeAnimation());
+    }
+
+    public IEnumerator TakeAnimation()
+    {
+        Vector2 startPosition = transform.position;
+        Vector2 endPosition = ClickableObjectManager.instance.GetItemTargetPos;
+
+        for (float t = 0; t < _takeAnimationTime; t += Time.deltaTime) {
+            transform.position = Vector2.Lerp(startPosition, endPosition, t / _takeAnimationTime);
+            yield return null;
+        }
+        gameObject.SetActive(false);
+        if (item)
+            PlayerInventory.instance.AddItem(item);
+    }
+
+    public void Inspect()
+    {
+        Debug.Log(name + " Inspected");
+    }
+    #endregion
+
+    #region HOVER_SYSTEM
+    public bool IsHovered
+    {
+        get => _isHovered;
+    }
+
+    private void OnMouseOver()
+    {
+        if (!IsInteractable || ClickableObjectManager.instance.CursorOnChoicePanel)
+            return;
+        HoverOn();
+    }
+
+    private void OnMouseExit()
+    {
+        if (!IsInteractable)
+            return;
+        HoverOff();
+    }
+
+    private void HoverOff()
+    {
+        _isHovered = false;
+        if (highlightObject)
+            highlightObject.SetActive(false);
+    }
+
+    private void HoverOn()
+    {
+        _isHovered = true;
+        if (highlightObject)
+            highlightObject.SetActive(true);
+    }
+    #endregion
+}
