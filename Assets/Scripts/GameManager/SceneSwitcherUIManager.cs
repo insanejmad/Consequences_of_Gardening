81024d16e22bad30b9db166823b74de9d7f10fb4
Kévin Reilhac <kreilhac@xeno-bits.com>
2020-04-25 03:18:10 +0200

    FIX: remove current scene button on SceneSwitcher
    CHANGE: SceneSwither UI disapear when user click outside or change scene

--- Assets/Scripts/GameManager/SceneSwitcherUIManager.cs
+++ Assets/Scripts/GameManager/SceneSwitcherUIManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 [System.Serializable]
 public class SceneItem
@@ -14,12 +15,13 @@ public class SceneSwitcherUIManager : MonoBehaviour
 {
 
     public static SceneSwitcherUIManager instance = null;
-
     public List<SceneItem> ScenesList;
-
-    public GameObject container;
-
+    public RectTransform panelButton;
+    public RectTransform container;
     public GameObject ButtonPrefab;
+
+    private List<SceneChangeButton> _buttons = new List<SceneChangeButton>();
+    private Canvas _canvas = null;
 
     void Awake()
     {
@@ -32,24 +34,58 @@ public class SceneSwitcherUIManager : MonoBehaviour
         {
             Destroy(gameObject);
         }
-
+        _canvas = GetComponentInParent<Canvas>();
         InitializeButtons();
-
+        UpdateButtons();
+        SceneManager.sceneLoaded += (Scene scene, LoadSceneMode mode) => UpdateButtons();
     }
 
-    public void InitializeButtons()
+    private void Update()
+    {
+        if (container.gameObject.activeSelf && IsClickOutside(container) && IsClickOutside(panelButton))
+            container.gameObject.SetActive(false);
+    }
+
+    private void UpdateButtons()
+    {
+        string currentScene = SceneManager.GetActiveScene().name;
+
+        foreach (var item in _buttons) {
+            if (item.SceneName == currentScene)
+                item.gameObject.SetActive(false);
+            else if (!item.gameObject.activeSelf)
+                item.gameObject.SetActive(true);
+        }
+        container.gameObject.SetActive(false);
+    }
+
+     private bool IsClickOutside(RectTransform p_rectTransform)
+     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (!RectTransformUtility.RectangleContainsScreenPoint(p_rectTransform, Input.mousePosition, null))
+                return (true);
+        }
+        return (false);
+    }
+
+    private void InitializeButtons()
     {
         foreach (var item in ScenesList)
         {
-	    Image img = null;
-            GameObject obj = Instantiate(ButtonPrefab) as GameObject;
-	    img = obj.GetComponent<Image>();
-	    img.preserveAspect = true;
-	    img.sprite = item.icon;
-            obj.GetComponent<SceneChangeButton>().SceneName = item.name;
-            obj.transform.SetParent(container.transform, false);
+            GameObject obj = Instantiate(ButtonPrefab, container.transform);
+            Image img = obj.GetComponent<Image>();
+            Button btn = obj.GetComponent<Button>();
+            SceneChangeButton changeSceneBtn = obj.GetComponent<SceneChangeButton>();
+
+            img.preserveAspect = true;
+            img.sprite = item.icon;
+            btn.onClick.AddListener(UpdateButtons);
+            changeSceneBtn.SceneName = item.name;
+            obj.name = item.name + " button";
+            _buttons.Add(changeSceneBtn);
         }
-        container.SetActive(false);
+        container.gameObject.SetActive(false);
     }
 
 }
